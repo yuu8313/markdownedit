68277290708201fb0eb4194fe5d248dfc26058d8
yuu8313 <yuyuyunoyuusuke.2@gmail.com>
2024-10-15 12:27:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,12 +32,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 blob = new Blob([marked(content)], {type: 'text/html'});
                 filename = 'document.html';
                 break;
-            case 'txt':
-                blob = new Blob([content], {type: 'text/plain'});
-                filename = 'document.txt';
+            case 'svg':
+                blob = new Blob([marked(content)], {type: 'text/svg'});
+                filename = 'document.html';
                 break;
             case 'txt':
-                blob = new Blob([content], {type: 'text/svg'});
+                blob = new Blob([content], {type: 'text/plain'});
                 filename = 'document.txt';
                 break;
             case 'csv':
